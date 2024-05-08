6a0cfe012ea6e857d97d5b834cfd2c831ad57ed9
danieljardine04 <danogaryjardine@gmail.com>
2024-05-08 14:01:54 -0400

    Set up Constructors with getters and setters

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac = new Fraction();
+        Fraction frac1 = new Fraction(5);
+        Fraction frac2 = new Fraction(6, 7);
+
+
+        Console.WriteLine(frac.getTop());
+        Console.WriteLine(frac1.getTop());
+        Console.WriteLine(frac2.getTop());
+        Console.WriteLine(frac2.getBottom());
+
+        frac.setBottom(2);
+        frac1.setBottom(6);
+
+        Console.WriteLine(frac.GetFractionString());
+        Console.WriteLine(frac.GetDecimalValue());
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac.GetDecimalValue());
+        
     }
 }
